408e3d5a1005d650b7e5f62ffc73a91aa8a3d6f3
cdhofer <chofer@cosy.sbg.ac.at>
2018-03-02 10:27:43 +0100

    * added transforms

--- chofer_tda_datasets/__init__.py
+++ chofer_tda_datasets/__init__.py
@@ -1,3 +1,4 @@
 from .nips_2017 import Animal, Mpeg7, Reddit_5K, Reddit_12K
 from .anon_eigenvalue_predict import AnonEigenvaluePredict
 from .reininghaus_2014 import *
+
---
+++ chofer_tda_datasets/utils/transforms.py
@@ -0,0 +1,38 @@
+import h5py
+
+
+class Hdf5GroupListSelector:
+    def __init__(self, keys: [str]):
+        self.keys = keys
+
+    def __call__(self, data_grp)->[]:
+        assert isinstance(data_grp, h5py.Group)
+        return [data_grp[key].value for key in self.keys]
+
+
+class Hdf5GroupToDict:
+    def __init__(self):
+        pass
+
+    def __call__(self, data_grp: h5py.Group):
+        assert isinstance(data_grp, (h5py.Group, h5py.Dataset))
+        if isinstance(data_grp, h5py.Dataset):
+            return data_grp.value
+        else:
+            return {k: self(v) for k, v in data_grp.items()}
+
+
+class Hdf5GroupToDictSelector:
+    def __init__(self, key_selection: {str}):
+        self.key_selection = key_selection
+
+    def __select(self, data_grp, selection):
+        if isinstance(selection, dict):
+            return {k: self.__select(data_grp[k], v) for k, v in selection.items()}
+        else:
+            assert isinstance(selection, (list, str))
+            return {k: data_grp[k].value for k in selection}
+
+    def __call__(self, data_grp: h5py.Group):
+        assert isinstance(data_grp, h5py.Group)
+        return self.__select(data_grp, self.key_selection)
