d1da359e43bb09d89b2309580e89e68ab2c37b99
cdhofer <chofer@cosy.sbg.ac.at>
2018-04-04 08:59:11 +0200

    * added AnonEigenvaluePredictSmall dataset

--- chofer_tda_datasets/anon_eigenvalue_predict.py
+++ chofer_tda_datasets/anon_eigenvalue_predict.py
@@ -3,3 +3,7 @@ from .utils.h5py_dataset import Hdf5SupervisedDatasetOneFile
 
 class AnonEigenvaluePredict(Hdf5SupervisedDatasetOneFile):
     file_name = 'anon_eigenvalue_predict_pershom_degree_filtration.h5'
+
+
+class AnonEigenvaluePredictSmall(Hdf5SupervisedDatasetOneFile):
+    file_name = 'anon_eigenvalue_predict_small_pershom_degree_filtration.h5'
